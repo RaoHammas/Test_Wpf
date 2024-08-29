41e8cb8b1330bee685f09ea8314cc02baa8046a4
Rao Hammas <125863679+RaoHammas-Logiciel@users.noreply.github.com>
2024-08-29 21:30:46 +0500

    Add project files.

---
+++ Test_Wpf/DataChannel.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Channels;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Test_Wpf;
+
+public static class DataChannel
+{
+    private static readonly Dictionary<string, Subscription<int>> Subscriptions;
+
+    static DataChannel()
+    {
+        Subscriptions = new Dictionary<string, Subscription<int>>();
+    }
+
+    public static ChannelReader<int> Subscribe(string key)
+    {
+        if (Subscriptions.TryGetValue(key, out var existingSubscription))
+        {
+            return existingSubscription.DataChannel.Reader;
+        }
+
+        var newSub = new Subscription<int>
+        {
+            Key = key,
+            DataChannel = Channel.CreateUnbounded<int>()
+        };
+
+        Task.Run(async () =>
+        {
+            var i = 0;
+            while (true)
+            {
+                newSub.DataChannel.Writer.TryWrite(++i);
+                await Task.Delay(500);
+            }
+        });
+
+        Subscriptions.Add(key, newSub);
+        return Subscriptions[key].DataChannel.Reader;
+    }
+}
+
+public partial class Subscription<T> : ObservableObject
+{
+    [ObservableProperty] private Channel<T> _dataChannel = null!;
+    [ObservableProperty] private string _key = null!;
+}
---
+++ Test_Wpf/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Test_Wpf;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty] private int _dataValue;
+
+    public MainWindowViewModel()
+    {
+        _ = SubscribeForData();
+    }
+
+    private Task SubscribeForData()
+    {
+        _ = Task.Run(async () =>
+        {
+            while (await DataChannel.Subscribe("IntData").WaitToReadAsync())
+            {
+                DataValue = await DataChannel.Subscribe("IntData").ReadAsync();
+            }
+        });
+
+        return Task.CompletedTask;
+        /*await foreach (var data in DataChannel.Subscribe("IntData").ReadAllAsync())
+        {
+            DataValue = data;
+        }*/
+    }
+
+    [RelayCommand]
+    private void OpenNewWindow()
+    {
+        var newWind = new MainWindow();
+        newWind.Show();
+    }
+}
